d8b41d4eba3798a0e36f8d7f424cc0620c34e1da
accueiltaka-dev <accueiltaka@gmail.com>
2025-09-13 14:23:01 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+get_header(); ?>
+
+<main id="site-content">
+    <section class="hero">
+        <h1>Bienvenue au Campus Cœur de Bourgogne</h1>
+        <p>Former les talents techniques pour un avenir durable</p>
+    </section>
+
+    <section class="content">
+        <h2>Contenu par défaut</h2>
+        <p>Ceci est la page d’index de votre thème. Vous pouvez personnaliser vos pages depuis l’administration WordPress.</p>
+    </section>
+</main>
+
+<?php
+get_footer();
